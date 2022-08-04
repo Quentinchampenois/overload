34724d1f76a0e66f3d92f2e2ace5c5e0569d0930
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-08-04 15:24:16 +0200

    Convert ignore files to vec of String

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn main() {
-    let filename = ".overloadignore";
-
+fn ignored_overloads(filename: &str) -> Vec<String> {
     let file = File::open(filename);
     if let Err(e) = file {
         println!("Error while reading file '{}' : {}", filename, e);
@@ -11,10 +9,14 @@ fn main() {
     }
 
     let reader = BufReader::new(file.unwrap());
-    let lines = reader.lines()
-        .map(|l| l.expect("Could not parse line"))
-        .collect::<Vec<String>>();
-    println!("{:?}", lines);
+    reader.lines()
+        .map(|l| format!("./{}", l.expect("Could not parse line")))
+        .collect::<Vec<String>>()
+}
+
+fn main() {
+    let ignored_files = ignored_overloads(".overloadignore");
+    println!("{:?}", ignored_files);
 
     std::process::exit(0);
 }
