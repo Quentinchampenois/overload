a9b0f452fd812ddf390b8a5093ef7a2c48381d4f
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-08-05 15:16:15 +0200

    Avoid duplicated overloads

--- src/commit.rs
+++ src/commit.rs
@@ -2,54 +2,15 @@
 pub struct Commit {
     pub hash: String,
     pub title: String,
-    pub filename: String
+    pub path: String
 }
 
-pub struct CommitGroup {
-    pub commit: Commit,
-    pub files: Vec<String>
-}
+impl Commit {
+    pub fn format(&self) -> String {
+        format!("* __{} - {}__
 
-#[derive(Debug)]
-pub struct Commits {
-    pub commits: Vec<Commit>
-}
+    ` {} `
 
-impl Commits {
-    pub fn find_by(&self, hash: String) -> Option<Commit> {
-        for mut commit in self.commits.iter() {
-            if commit.hash == hash {
-                return Some(commit.clone());
-            }
-        }
-        None
-    }
-
-    pub fn display_by_hash(&self, hash: String) -> String {
-        let mut group: Vec<Commit> = vec![];
-        for commit in &self.commits {
-            if commit.hash == hash {
-                group.push(commit.clone());
-            }
-        }
-
-        return self.format_display(group);
-    }
-
-    pub fn format_display(&self, commits: Vec<Commit>) -> String {
-        let mut result : String = format!("
-### {} - {}", commits[0].hash, commits[0].title).to_string();
-        for commit in commits {
-            let mut format = format!("
-* {}", commit.filename);
-            let s_slice: &str = &format[..];
-            result.push_str(s_slice);
-        }
-
-        return result.to_string();
-    }
-
-    pub fn add(&mut self, commit: Commit) {
-        &self.commits.push(commit);
+", self.hash, self.title, self.path).to_string()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod filesystem;
 use std::collections::HashSet;
 use std::fs;
 use std::process::Command;
-use commit::{Commits, Commit};
+use commit::{Commit};
 use filesystem as fss;
 use std::fs::OpenOptions;
 use std::io::prelude::*;
@@ -59,33 +59,49 @@ fn main() {
         dir_files.push(entry_path);
     }
 
-    println!("{:?}", dir_files);
-    /*
-    excluded.append(&mut vec![
-        String::from(".git"),
-        String::from(".idea"),
-        String::from("OVERLOADS.md"),
-        String::from("packages"),
-        String::from("public"),
-        String::from("tmp"),
-        String::from("vendor"),
-        String::from("docs"),
-        String::from("deploy/providers"),
-    ]);
+    let mut overload_file = fss::find_or_create_file("OVERLOADS.md");
 
-    let mut file = fss::find_or_create_file("OVERLOADS.md");
-    let mut commits : Commits = Commits { commits: vec![] };
-    let mut overloads : Vec<String> = Vec::new();
+    let mut buffer_reader = String::new();
+    overload_file.read_to_string(&mut buffer_reader);
+    let mut clone_buf_reader = buffer_reader.clone();
 
-    let walker = WalkDir::new(".").into_iter();
-    for entry in walker.filter_entry(|e| !is_hidden(e, excluded.clone())) {
-        let entry = entry.unwrap();
-        let filename = entry.file_name().to_string_lossy().parse().unwrap();
-        if filename == "." || filename == ".." { continue; }
+    for path in dir_files {
+        let output = Command::new("git")
+            .arg("log")
+            .arg("-n 1")
+            .arg("--pretty=format:%C(auto)%h :: %as :: %an :: %s")
+            .arg("--")
+            .arg(&path)
+            .output()
+            .expect("failed to execute process");
 
-        overloads.push(filename);
+        let commit_msg = std::str::from_utf8(&output.stdout).unwrap();
+        let vec: Vec<&str> = commit_msg.split("::").collect();
+
+        // Continue if file isn't in Git history
+        if commit_msg == "" { continue; }
+
+        let commit = Commit {
+            hash: String::from(vec[0].trim()),
+            title: String::from(vec[vec.len() - 1].trim()),
+            path: String::from(&path),
+        };
+
+        println!("buf reader contains {} ? {}", &commit.path, clone_buf_reader.contains(&commit.path));
+        if clone_buf_reader.contains(&commit.path) { continue; }
+        clone_buf_reader.push_str(&*commit.format());
+        println!("buf updated {} ? {}", &commit.path, clone_buf_reader.contains(&commit.path));
     }
 
+    if buffer_reader != clone_buf_reader {
+        println!("buffer differents");
+        if let Err(e) = writeln!(overload_file, "{}", clone_buf_reader) {
+            eprintln!("Couldn't write in file: {}", e);
+        }
+    }
+
+
+    /*
     for overload in overloads {
         let target = overload;
         let output = Command::new("git")
@@ -126,6 +142,7 @@ fn main() {
     if let Err(e) = writeln!(file, "{}", buffer_reader) {
         eprintln!("Couldn't write in file: {}", e);
     }
-*/
+
+     */
     std::process::exit(0);
 }
